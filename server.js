6362b52050270fb1b1d93612018a81ef2208c189
Mohamed Abbassi <mabbassix3@gmail.com>
2020-06-08 20:28:06 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const flash = require('express-flash');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (request, response) =>  response.sendFile(`${__dirname}/log-in.html`));
+app.get('/', (request, response) =>  response.sendFile(`${__dirname}/sign-up.html`));
+
+app.post('https://api.netalem.tn/users', (request, response) => {
+  const postBody = request.body;
+  response.send();
+  console.log(postBody);
+});
+
+
+const middlewares = [
+    // ...
+    cookieParser(),
+    session({
+      secret: 'super-secret-key',
+      key: 'super-secret-cookie',
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 60000 }
+    }),
+    flash(),
+  ];
+
+
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!");
+  });
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+
+app.listen(3000, () => console.info('Application running on port 3000'));
